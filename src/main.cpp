f96c78ea3825bc784ffb665389dc9d6ae8067762
tthomas2000 <tthomas23@my.whitworth.edu>
2022-01-11 11:35:10 -0800

    Updated main().

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <mbed.h>
+#include "HostIO.h"
+#include "SegDisplay.h"
+
 
 int main() {
-
-  // put your setup code here, to run once:
-
+  char data1, data2;
+  SegInit(); //Initializes the 7-Segment Display
+  HostInit(); //Initializes communication w/ the host computer
+  
   while(1) {
-    // put your main code here, to run repeatedly:
+    data2 = GetKeyInput();
+    Seg2 = SegConvert(data2);
+    
+    data1 = GetKeyInput();
+    Seg1 = SegConvert(data1);
+
+    printf("  "); //Display spaces on host after recieving 2 digits
   }
 }
