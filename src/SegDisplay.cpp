9a7de0fa4dbc0d97e2165bc488b209de830673b9
tthomas2000 <tthomas23@my.whitworth.edu>
2022-01-11 12:21:19 -0800

    Done

--- src/SegDisplay.cpp
+++ src/SegDisplay.cpp
@@ -3,7 +3,7 @@
 BusOut Seg1(p5, p6, p7, p8, p9, p10, p11); //a-g (no dp)
 BusOut Seg2(p13, p14, p15, p16, p17, p18, p19); //a-g (no dp)
 
-void SegIniti(void) {
+void SegInit(void) {
     Seg1 = SegConvert(0);
     Seg2 = SegConvert(0);
 }
